394109d4f13f854648e5486c253cc5828e3496af
tranminhtoan149 <tranminhtoan.149@gmail.com>
2024-03-08 22:53:36 +0700

    add header and some module

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,8 +1,44 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { Col, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    const landingPages = [
+        {
+            title: 'Trang chủ',
+            url: '/',
+        },
+        {
+            title: 'Về Chúng Tôi',
+            url: '/about',
+        },
+        {
+            title: 'Lớp Học',
+            url: '/course',
+        },
+    ];
+    return (
+        <div className={cx('header-wrapper')}>
+            <Row className={cx('contact-infor')}>
+                <Col xs={8}></Col>
+                <Col xs={2}>0981710152</Col>
+                <Col xs={2}>
+                    <FontAwesomeIcon icon="fa-regular fa-envelope" />
+                    tranminhtoan.149@gmail.com
+                </Col>
+            </Row>
+            <Row className={cx('header')}>
+                <Col className={cx('logo')} xs={2}>
+                    Logo
+                </Col>
+                <Col xs={8}></Col>
+                <Col className={cx('auth-btn')} xs={2}>
+                    Login/Register
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Header;

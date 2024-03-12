2fa1f0b6bfacd4a155b8f95b7d71e8535a6158fa
MinhToan_Mystic <mystic_tmtoan>
2024-03-12 16:52:40 +0700

    update ui for homepage

---
+++ src/pages/Home/WhyChoose.jsx
@@ -0,0 +1,53 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+import { Button, Grid } from '@mui/material';
+import { Check } from '@mui/icons-material';
+const cx = classNames.bind(styles);
+
+function WhyChoose() {
+    return (
+        <Grid
+            container
+            spacing={2}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            rowSpacing={2}
+            className={cx('why-chooese')}
+        >
+            <Grid item xs={6} className={cx('why-choose')}>
+                <div className={cx('title')}>
+                    <div>Tại sao chọn</div>
+                    <h2>Lý do chọn StarKids</h2>
+                </div>
+                <ul className={cx('list-items')}>
+                    <li className={cx('item')}>
+                        <Check />
+                        <span>Ưu tiên hàng đầu sự an toàn của trẻ</span>
+                    </li>
+                    <li className={cx('item')}>
+                        <Check />
+                        <span>Cung cấp chương trình học thuật cấp tiến nhất</span>
+                    </li>
+                    <li className={cx('item')}>
+                        <Check />
+                        <span>Chăm sóc chu đáo về dinh dưỡng và sức khỏe</span>
+                    </li>
+                    <li className={cx('item')}>
+                        <Check />
+                        <span>Đầu tư chuyên môn cho đội ngũ giáo viên ưu tú</span>
+                    </li>
+                    <li className={cx('item')}>
+                        <Check />
+                        <span>Tối ưu cơ sở vật chất và đổi mới công cụ giảng dạy</span>
+                    </li>
+                </ul>
+
+                <Button variant="contained" className={cx('button')}>
+                    Xem Thêm
+                </Button>
+            </Grid>
+            <Grid item xs={6}></Grid>
+        </Grid>
+    );
+}
+
+export default WhyChoose;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,26 @@
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
+import Banner from './Banner';
+import Admissions from './Admissions';
+import News from './News';
+import WhyChoose from './WhyChoose';
+import TeacherTeam from './TeacherTeam';
+import { Divider } from '@mui/material';
 const cx = classNames.bind(styles);
 
 function Home() {
-    return <div className={cx('content')}>HomePage</div>;
+    return <div className={cx('container')}>
+        <Banner />
+        <Divider />
+        <Admissions />
+        <Divider />
+        <News />
+        <Divider />
+        <WhyChoose />
+        <Divider />
+        <TeacherTeam />
+
+    </div>;
 }
 
 export default Home;

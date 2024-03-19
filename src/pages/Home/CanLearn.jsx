89c74255912e897592bb5bdc6b3016bb7f421dfa
MinhToan_Mystic <mystic_tmtoan>
2024-03-19 15:26:47 +0700

    update homePage

--- src/pages/Home/CanLearn.jsx
+++ src/pages/Home/CanLearn.jsx
@@ -1,61 +1,76 @@
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
 import 'react-multi-carousel/lib/styles.css';
-import { Card, CardMedia, Grid } from '@mui/material';
+import { Card, CardContent, CardMedia, Grid, Rating } from '@mui/material';
 const cx = classNames.bind(styles);
 
 function CanLearn() {
     const CardNews = ({ image }) => (
-        <Card sx={{ maxWidth: 300 }} className={cx('card')}>
+        <Card sx={{ maxWidth: 350 }} className={cx('card')}>
             <CardMedia sx={{}} image={image.imgPath} className={cx('image')} title="" />
-            <div className={cx('card-content')}></div>
+            <CardContent className={cx('card-content')}>
+                <div className={cx('content-info')}>
+                    <span className={cx('tag-class')}>Lớp học</span>
+                    <div style={{ marginTop: 10 }}>
+                        <Rating name="read-only" value={5} readOnly size="small" />
+                    </div>
+                    <h3 style={{ maxWidth: '55%' }}>
+                        <span>{image.class}</span>
+                    </h3>
+                </div>
+                <div className={cx('content-time')}>
+                    Từ ngày {image.fromDate} đến ngày {image.toDate}
+                </div>
+            </CardContent>
         </Card>
     );
     const images = [
         {
-            imgPath: 'https://www.w3schools.com/howto/img_avatar.png',
+            imgPath: 'images/canlearn.webp',
             class: 'Lớp 13 tới 18 tháng',
             fromDate: '13/4/2023',
             toDate: '30/10/2023',
         },
-        // {
-        //     imgPath: 'https://www.w3schools.com/howto/img_avatar.png',
-        //     class: 'Lớp 13 tới 18 tháng',
-        //     fromDate: '13/4/2023',
-        //     toDate: '30/10/2023',
-        // },
-        // {
-        //     imgPath: 'https://www.w3schools.com/howto/img_avatar.png',
-        //     class: 'Lớp 13 tới 18 tháng',
-        //     fromDate: '13/4/2023',
-        //     toDate: '30/10/2023',
-        // },
-        // {
-        //     imgPath: 'https://www.w3schools.com/howto/img_avatar.png',
-        //     class: 'Lớp 13 tới 18 tháng',
-        //     fromDate: '13/4/2023',
-        //     toDate: '30/10/2023',
-        // },
-        // {
-        //     imgPath: 'https://www.w3schools.com/howto/img_avatar.png',
-        //     class: 'Lớp 13 tới 18 tháng',
-        //     fromDate: '13/4/2023',
-        //     toDate: '30/10/2023',
-        // },
-        // {
-        //     imgPath: 'https://www.w3schools.com/howto/img_avatar.png',
-        //     class: 'Lớp 13 tới 18 tháng',
-        //     fromDate: '13/4/2023',
-        //     toDate: '30/10/2023',
-        // },
+        {
+            imgPath: 'images/canlearn.webp',
+            class: 'Lớp 13 tới 18 tháng',
+            fromDate: '13/4/2023',
+            toDate: '30/10/2023',
+        },
+        {
+            imgPath: 'images/canlearn.webp',
+            class: 'Lớp 13 tới 18 tháng',
+            fromDate: '13/4/2023',
+            toDate: '30/10/2023',
+        },
+        {
+            imgPath: 'images/canlearn.webp',
+            class: 'Lớp 13 tới 18 tháng',
+            fromDate: '13/4/2023',
+            toDate: '30/10/2023',
+        },
+        {
+            imgPath: 'images/canlearn.webp',
+            class: 'Lớp 13 tới 18 tháng',
+            fromDate: '13/4/2023',
+            toDate: '30/10/2023',
+        },
+        {
+            imgPath: 'images/canlearn.webp',
+            class: 'Lớp 13 tới 18 tháng',
+            fromDate: '13/4/2023',
+            toDate: '30/10/2023',
+        },
     ];
 
     return (
         <div className={cx('can-learn')}>
             <div className={cx('container')}>
                 <div className={cx('title')}>
-                    {/* <div>Học tập</div> */}
-                    <h2>{/* <span>Bé học gì ở StarKids</span> */}</h2>
+                    <div>LỚP HỌC</div>
+                    <h2>
+                        <span>Bé học gì ở StarKids</span>
+                    </h2>
                 </div>
                 <Grid
                     container

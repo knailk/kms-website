812f93700ebf69c40cfe2025f1696ec5aa4c9740
MinhToan_Mystic <mystic_tmtoan>
2024-03-29 16:27:12 +0700

    update source

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,7 +3,7 @@ import { Avatar as AvatarMui } from '@mui/material';
 function Avatar({ src, name, width = 50, height = 50, className }) {
     return (
         <>
-            {src && <AvatarMui sx={{ width: 50, height: 50 }} src={src} alt={name} />}
+            {src && <AvatarMui sx={{ width: width, height: height }} src={src} alt={name} />}
             {!src && (
                 <AvatarMui sx={{ width: width, height: height, backgroundColor: '#0072cd' }}>
                     {name ? name.charAt(0) : ''}

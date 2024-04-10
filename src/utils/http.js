97bf1be396502b11d0616449e24b96b793bf79a1
Nguyen Danh Tien Dung <tiendung3719@gmail.com>
2024-04-10 11:59:22 +0700

    update: change http request

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,19 +1,8 @@
 import axios from 'axios';
 
-const config = {
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
-};
 const request = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/',
-    withCredentials: false,
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
+    baseURL: 'http://localhost:8080/api/v1',
+    withCredentials: true, 
 });
 
 export default request;

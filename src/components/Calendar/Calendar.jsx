fc57240fff2b826423e46667fb7afe30e01cf92c
Nguyen Danh Tien Dung <tiendung3719@gmail.com>
2024-04-23 18:04:53 +0700

    feat: add calendar

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import Dialog from '@mui/material/Dialog';
+import Button from '@material-ui/core/Button';
+import TextField from '@mui/material/TextField';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+require('react-big-calendar/lib/css/react-big-calendar.css');
+
+BigCalendar.momentLocalizer(moment);
+
+function Calendar() {
+    const eventActions = [
+        <Button label="Cancel" primary={false} keyboardFocused={true} onClick={this.handleClose} />,
+        <Button
+            label="Delete"
+            secondary={true}
+            keyboardFocused={true}
+            // onClick={() => {
+            //     this.deleteEvent(), this.handleClose();
+            // }}
+        />,
+        <Button
+            label="Confirm Edit"
+            primary={true}
+            keyboardFocused={true}
+            onClick={this.handleClose}
+            // onClick={() => {
+            //     this.updateEvent(), this.handleClose();
+            // }}
+        />,
+    ];
+    const appointmentActions = [
+        <Button label="Cancel" secondary={true} onClick={this.handleClose} />,
+        <Button
+            label="Submit"
+            primary={true}
+            keyboardFocused={true}
+            // onClick={() => {
+            //     this.setNewAppointment(), this.handleClose();
+            // }}
+        />,
+    ];
+    return (
+        <div id="Calendar">
+            {/* react-big-calendar library utilized to render calendar*/}
+            <BigCalendar
+                events={this.state.events}
+                views={['month', 'week', 'day', 'agenda']}
+                timeslots={2}
+                defaultView="month"
+                defaultDate={new Date()}
+                selectable={true}
+                onSelectEvent={(event) => this.handleEventSelected(event)}
+                onSelectSlot={(slotInfo) => this.handleSlotSelected(slotInfo)}
+            />
+
+            {/* Material-ui Modal for booking new appointment */}
+            <Dialog
+                title={`Book an appointment on ${moment(this.state.start).format('MMMM Do YYYY')}`}
+                actions={appointmentActions}
+                modal={false}
+                open={this.state.openSlot}
+                onRequestClose={this.handleClose}
+            >
+                <TextField
+                    floatingLabelText="Title"
+                    onChange={(e) => {
+                        this.setTitle(e.target.value);
+                    }}
+                />
+                <br />
+                <TextField
+                    floatingLabelText="Description"
+                    onChange={(e) => {
+                        this.setDescription(e.target.value);
+                    }}
+                />
+                <TimePicker
+                    format="ampm"
+                    floatingLabelText="Start Time"
+                    minutesStep={5}
+                    value={this.state.start}
+                    onChange={this.handleStartTime}
+                />
+                <TimePicker
+                    format="ampm"
+                    floatingLabelText="End Time"
+                    minutesStep={5}
+                    value={this.state.end}
+                    onChange={this.handleEndTime}
+                />
+            </Dialog>
+
+            {/* Material-ui Modal for Existing Event */}
+            <Dialog
+                title={`View/Edit Appointment of ${moment(this.state.start).format('MMMM Do YYYY')}`}
+                actions={eventActions}
+                modal={false}
+                open={this.state.openEvent}
+                onRequestClose={this.handleClose}
+            >
+                <TextField
+                    defaultValue={this.state.title}
+                    floatingLabelText="Title"
+                    onChange={(e) => {
+                        this.setTitle(e.target.value);
+                    }}
+                />
+                <br />
+                <TextField
+                    floatingLabelText="Description"
+                    multiLine={true}
+                    defaultValue={this.state.desc}
+                    onChange={(e) => {
+                        this.setDescription(e.target.value);
+                    }}
+                />
+                <TimePicker
+                    format="ampm"
+                    floatingLabelText="Start Time"
+                    minutesStep={5}
+                    value={this.state.start}
+                    onChange={this.handleStartTime}
+                />
+                <TimePicker
+                    format="ampm"
+                    floatingLabelText="End Time"
+                    minutesStep={5}
+                    value={this.state.end}
+                    onChange={this.handleEndTime}
+                />
+            </Dialog>
+        </div>
+    );
+}
+
+export default Calendar;
--- src/pages/AdminManagement/Schedule/index.jsx
+++ src/pages/AdminManagement/Schedule/index.jsx
@@ -1,7 +1,84 @@
 import * as React from 'react';
 
+import classNames from 'classnames/bind';
+import 'rsuite/dist/rsuite.min.css';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import { Button } from '@mui/material';
+import { LoggedContext } from '~/components/Layout/LoggedLayout';
+import { Calendar } from '~/components/Calendar/Calendar';
+import request from '~/utils/http';
+import SchoolIcon from '@mui/icons-material/School';
 import styles from './Schedule.module.scss';
 
+const cx = classNames.bind(styles);
+
 export default function Schedule() {
-    return <div>Schedule </div>;
+    const context = React.useContext(LoggedContext);
+    const [classes, setClasses] = React.useState([]);
+    const [classSelected, setClassSelected] = React.useState({});
+
+    React.useEffect(() => {
+        request
+            .get(`admin/class?page=1&limit=10`)
+            .then((response) => {
+                setClasses(response.data.classes);
+                setClassSelected(response.data.classes[0]);
+            })
+            .catch((error) => {
+                context.setShowSnackbar('Không tìm thấy thông tin lớp học', 'error');
+            });
+    }, []);
+
+    return (
+        <>
+            <h2 className={cx('title')}>Thời gian biểu lớp học</h2>
+            <div className={cx('classes-wrapper')}>
+                <div style={{ alignItems: 'center', display: 'flex' }}>
+                    <FormControl sx={{ m: 1, minWidth: 240 }} size="small">
+                        <InputLabel id="demo-simple-select-label">Chọn lớp</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={classSelected}
+                            label="Chọn lớp"
+                            onChange={(event) => {
+                                setClassSelected(event.target.value);
+                            }}
+                        >
+                            {classes.map((classMap) => (
+                                <MenuItem key={classMap.id} value={classMap}>
+                                    {classMap.className}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Button variant="contained" startIcon={<SchoolIcon />}>
+                        Tạo lớp mới
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant="contained"
+                        startIcon={<PersonRemoveIcon />}
+                        color="error"
+                        style={{ marginRight: '5px' }}
+                    >
+                        Xoá thành viên
+                    </Button>
+
+                    <Button variant="contained" startIcon={<PersonAddAlt1Icon />}>
+                        Thêm thành viên
+                    </Button>
+                </div>
+                <div style={{ height: 'calc(90vh - 196px)', width: '100%' }}>
+                    <Calendar />
+                </div>
+            </div>
+        </>
+    );
 }
